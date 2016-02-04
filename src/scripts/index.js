ed7b377f15127567409f3af06179f3ffafc24a7b
timwis <tim@timwis.com>
2016-02-03 19:58:44 -0500

    Use distinct field-container view

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,5 @@
-import LayerModel from './models/layer-model'
-import FieldListView from './views/field-list-view'
+import FieldContainerView from './views/field-container-view'
 
-const defaultServiceUrl = 'https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/Incidents_2006/FeatureServer/0'
-const layer = new LayerModel(null, {serviceUrl: defaultServiceUrl})
-layer.fetch()
-
-const fieldListView = new FieldListView({model: layer})
-document.querySelector('#fields').appendChild(fieldListView.render().el)
+var fieldContainerView = new FieldContainerView()
+fieldContainerView.serviceUrl = 'https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/Incidents_2006/FeatureServer/0'
+document.querySelector('#main').appendChild(fieldContainerView.render().el)
