2310c20d9ff1d77c598f6c66d1668090e27627da
timwis <tim@timwis.com>
2016-02-07 15:31:32 -0500

    remove sample result from each field

--- src/scripts/views/field-panel-item-view.js
+++ src/scripts/views/field-panel-item-view.js
@@ -1,47 +1,19 @@
 import View from 'ampersand-view'
-import $ from 'jquery'
 
 import EventBus from '../event-bus'
 import FieldPanelItemTemplate from '../templates/field-panel-item.html'
 
 const FieldPanelItemView = View.extend({
   template: FieldPanelItemTemplate,
-  props: {
-    'sampleResult': 'object'
-  },
   bindings: {
-    'sampleResult': {
-      'hook': 'sample-result',
-      'type': (el, value, previousValue) => {
-        if (value) el.innerText = JSON.stringify(value, null, 2)
-      }
-    },
-    'model.sampleValue': [
-      {
-        type: 'text',
-        hook: 'sample-value'
-      },
-      {
-        type: 'attribute',
-        name: 'placeholder',
-        hook: 'filter-value'
-      }
-    ],
-    'model.sampleUrl': [
-      {
-        type: 'text',
-        hook: 'sample-url'
-      },
-      {
-        type: 'attribute',
-        name: 'href',
-        hook: 'sample-url'
-      }
-    ]
+    'model.sampleValue': {
+      type: 'attribute',
+      name: 'placeholder',
+      hook: 'filter-value'
+    }
   },
   events: {
     'click [data-toggle="collapse"]': 'onToggle',
-    'click [data-hook=sample-url]': 'onClickSampleUrl',
     'change [data-hook=filter-operator]': 'onChangeFilterValue',
     'change [data-hook=filter-value]': 'onChangeFilterValue'
   },
@@ -49,11 +21,6 @@ const FieldPanelItemView = View.extend({
     if (!this.model.sampleValue) {
       this.model.getSampleValue().then((sampleValue) => this.model.sampleValue = sampleValue)
     }
-    e.preventDefault()
-  },
-  onClickSampleUrl: function (e) {
-    this.queryByHook('sample-result').style.display = 'block'
-    $.getJSON(this.model.sampleUrl, (result) => this.sampleResult = result)
     e.preventDefault()
   },
   onChangeFilterValue: function (e) {
