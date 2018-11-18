6068db4dfdb5d1fbcf8690927573fecece0c219d
timwis <tim@timwis.com>
2018-11-18 17:14:29 -0500

    Update sample URL

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,7 @@ import LayerModel from './models/layer-model'
 import QueryModel from './models/query-model'
 import PageLayoutView from './views/page-layout-view'
 
-const defaultServiceUrl = 'https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/Incidents_2006/FeatureServer/0'
+const defaultServiceUrl = 'https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/INCIDENTS_PART1_PART2/FeatureServer/0'
 const query = new QueryModel(null, {serviceUrl: defaultServiceUrl})
 const layer = new LayerModel(null, {serviceUrl: defaultServiceUrl})
 layer.fetch()
